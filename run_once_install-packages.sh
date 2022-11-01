02025c15a20c3a8390da86b249de46cf8ae3ef12
Vermium Sifell <vermium@zyner.org>
2022-11-02 00:24:59 +0100

    Added lightdm

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo pacman -S feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl
+sudo pacman -S feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl lightdm lightdm-gtk-greeter
 ~/.emacs.d/bin/doom install
