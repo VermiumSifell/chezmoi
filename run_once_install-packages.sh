461e465bbc57650cbbec23dfb7e94b7666612083
Vermium Sifell <vermium@zyner.org>
2022-11-02 00:26:09 +0100

    Enable lightdm

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sudo pacman -S feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl lightdm lightdm-gtk-greeter
 ~/.emacs.d/bin/doom install
+sudo systemctl enable --now lightdm
