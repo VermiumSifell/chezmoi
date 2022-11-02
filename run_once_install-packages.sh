2b5afd2797e4240157120e26f34c17946d3d3b91
Vermium Sifell <vermium@zyner.org>
2022-11-02 14:48:23 +0100

    Add meson and mu

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-sudo pacman -S feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl pipewire wireplumber pipewire-pulse ttf-fira-code
+sudo pacman -S meson feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl pipewire wireplumber pipewire-pulse ttf-fira-code
 systemctl enable --user --now pipewire.socket
 systemctl enable --user --now pipewire-pulse.socket
+
+
+
+paru -S mu
