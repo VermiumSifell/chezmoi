96277a3e485564452f71ef375cdacfc41776a828
Vermium Sifell <vermium@zyner.org>
2022-11-02 14:19:37 +0100

    Remove doom install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sudo pacman -S feh i3-gaps polybar alacritty zsh emacs ttf-font-awesome slock playerctl brightnessctl pipewire wireplumber pipewire-pulse ttf-fira-code
-~/.emacs.d/bin/doom install
 systemctl enable --user --now pipewire.socket
 systemctl enable --user --now pipewire-pulse.socket
