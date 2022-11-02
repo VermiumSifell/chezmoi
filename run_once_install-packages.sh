be378f77fa82e2155d6ae0612c303ee7e01248a0
Vermium Sifell <vermium@zyner.org>
2022-11-02 16:35:45 +0100

    Add missing rofi

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
-sudo pacman -S meson feh i3-gaps polybar alacritty zsh emacs slock playerctl brightnessctl
+sudo pacman -S meson rofi feh i3-gaps polybar alacritty zsh emacs slock playerctl brightnessctl
 paru -S mu
