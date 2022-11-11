233d7c637d7fde7f2ffaaff3182e6fe1cfab4251
Vermium Sifell <vermium@zyner.org>
2022-11-11 10:47:19 +0100

    Add more fonts

--- run_once_02-install-fonts.sh
+++ run_once_02-install-fonts.sh
@@ -1,2 +1,4 @@
 #!/bin/env sh
-sudo pacman --noconfirm -S ttf-font-awesome ttf-fira-code
+sudo pacman --noconfirm -S ttf-font-awesome ttf-fira-code noto-fonts-emoji ttf-joypixels
+
+paru --noconfirm -S ttf-symbola otf-openmoji ttf-twemoji
