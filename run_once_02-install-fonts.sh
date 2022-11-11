a7dc15cc5f986b5dbde4dac08cf400ab603eb83e
Vermium Sifell <vermium@zyner.org>
2022-11-11 10:53:32 +0100

    Add ttf-liberation

--- run_once_02-install-fonts.sh
+++ run_once_02-install-fonts.sh
@@ -1,4 +1,4 @@
 #!/bin/env sh
-sudo pacman --noconfirm -S ttf-font-awesome ttf-fira-code noto-fonts-emoji ttf-joypixels
+sudo pacman --noconfirm -S ttf-font-awesome ttf-fira-code noto-fonts-emoji ttf-joypixels ttf-liberation
 
 paru --noconfirm -S ttf-symbola otf-openmoji ttf-twemoji
