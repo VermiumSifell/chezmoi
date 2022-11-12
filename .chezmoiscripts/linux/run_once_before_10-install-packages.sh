a7f35143b2ca243fa71e16513ecdd1ce1cdba3e6
Vermium Sifell <vermium@zyner.org>
2022-11-12 14:40:55 +0100

    Debug

--- .chezmoiscripts/linux/run_once_before_10-install-packages.sh
+++ .chezmoiscripts/linux/run_once_before_10-install-packages.sh
@@ -29,14 +29,14 @@ aur_packages=(
 #)
 #{{   end -}}
 
-{{   if .i3wm -}}
-packages+=(
-  i3-gaps
-  polybar
-  rofi
-  dunst
-)
-{{   end -}}
+#{{   if .i3wm -}}
+#packages+=(
+#  i3-gaps
+#  polybar
+#  rofi
+#  dunst
+#)
+#{{   end -}}
 
 #{{   if .swaywm -}}
 #packages+=(
@@ -61,38 +61,38 @@ sudo pacman -Syu --noconfirm
 pacman -Syu --noconfirm
 {{   end -}}
 
-for package in ${packages[@]}; do
-  if [ "$(pacman -Qq $package 2> /dev/null)" != $package ]; then
-    echo "installing $package"
-{{-   if ne .chezmoi.username "root" }}
-    sudo pacman -S --noconfirm $package
-{{-   else }}
-    pacman -S --noconfirm $package
-{{-   end }}
-  fi
-done
+#for package in ${packages[@]}; do
+#  if [ "$(pacman -Qq $package 2> /dev/null)" != $package ]; then
+#    echo "installing $package"
+#{{-   if ne .chezmoi.username "root" }}
+#    sudo pacman -S --noconfirm $package
+#{{-   else }}
+#    pacman -S --noconfirm $package
+#{{-   end }}
+#  fi
+#done
 
 ## Install paru
-if [ ! $(command -v paru) ]; then
-  echo "installing paru"
-{{-   if ne .chezmoi.username "root" }}
-  sudo pacman -S --needed --noconfirm git base-devel
-{{-   else}}
-  pacman -S --needed --noconfirm git base-devel
-{{-   end }}
-  git clone https://aur.archlinux.org/paru.git /tmp/paru
-  cd /tmp/paru
-  makepkg -si --noconfirm
-  cd -
-  rm -rf /tmp/paru
-fi
+#if [ ! $(command -v paru) ]; then
+#  echo "installing paru"
+#{{-   if ne .chezmoi.username "root" }}
+#  sudo pacman -S --needed --noconfirm git base-devel
+#{{-   else}}
+#  pacman -S --needed --noconfirm git base-devel
+#{{-   end }}
+#  git clone https://aur.archlinux.org/paru.git /tmp/paru
+#  cd /tmp/paru
+#  makepkg -si --noconfirm
+#  cd -
+#  rm -rf /tmp/paru
+#fi
 
-for aur_package in ${aur_packages[@]}; do
-  if [ $(pacman -Qq $aur_package 2> /dev/null) != $aur_package ]; then
-    echo "installing $aur_package from AUR"
-    paru -S --noconfirm $aur_package
-  fi
-done
+#for aur_package in ${aur_packages[@]}; do
+#  if [ $(pacman -Qq $aur_package 2> /dev/null) != $aur_package ]; then
+#    echo "installing $aur_package from AUR"
+#    paru -S --noconfirm $aur_package
+#  fi
+#done
 
 echo -e "\033[0;32m>>>>> Finish Setting Up Arch Linux Packages <<<<<\033[0m"
-{{ end -}}
+#{{ end -}}
