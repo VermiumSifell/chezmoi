eb4dbb8fb9f65b699f0b1363cc5d908432a5a574
Vermium Sifell <vermium@zyner.org>
2022-11-20 03:10:20 +0100

    fix more stuff

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,5 +1,16 @@
 #!/bin/env bash
 
+notify-send -u low "restarting polybar"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null
+do
+    sleep 1
+done
+
 ## The name of the bar you want to manage
 BAR_NAMES="panel"
 TRAY_OUTPUT=$(xrandr | awk '/ primary / {print $1}')
@@ -10,17 +21,18 @@ ACTIVE_MONITORS=$(xrandr | awk '/ connected/{print $1}')
 ## For each bar, do the following
 for bar in $BAR_NAMES; do
 
-    ## Kill all polybar processes
-    pkill -9 polybar
-
     for monitor in $ACTIVE_MONITORS; do
 
         if [[ "$monitor" == "$TRAY_OUTPUT" ]]; then
-            POLYBAR_TRAY_POS="right" MONITOR=$monitor polybar $bar &
+            POLYBAR_TRAY_POS="right" MONITOR=$monitor polybar $bar >> /tmp/polybar.log 2>&1 &
         else
-            POLYBAR_TRAY_POS="none" MONITOR=$monitor polybar $bar &
+            POLYBAR_TRAY_POS="none" MONITOR=$monitor polybar $bar >> /tmp/polybar.log 2>&1 &
         fi
 
     done
 
 done
+
+echo "Bars launched..."
+
+notify-send -u low "restarted polybar"
